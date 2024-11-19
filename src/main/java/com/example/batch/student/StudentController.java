bebbae8b0797cc9fca7bfdc54733a76d0b38ee50
Lahiru <lahiruviet@gmail.com>
2024-11-19 12:21:08 +0700

    initial development

---
+++ src/main/java/com/example/batch/student/StudentController.java
@@ -0,0 +1,37 @@
+package com.example.batch.student;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.*;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.batch.core.repository.JobExecutionAlreadyRunningException;
+import org.springframework.batch.core.repository.JobInstanceAlreadyCompleteException;
+import org.springframework.batch.core.repository.JobRestartException;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/v1/students")
+@RequiredArgsConstructor
+@Slf4j
+public class StudentController {
+
+    private final JobLauncher jobLauncher;
+    private final Job job;
+
+    @GetMapping
+    public void importCsvToDbJob() {
+        JobParameters jobParameters = new JobParametersBuilder()
+                .addLong("startAt", System.currentTimeMillis())
+                .toJobParameters();
+        try {
+            jobLauncher.run(job, jobParameters);
+        } catch (JobExecutionAlreadyRunningException
+                 | JobRestartException
+                 | JobInstanceAlreadyCompleteException
+                 | JobParametersInvalidException e) {
+            log.error(e.getMessage());
+        }
+    }
+}
